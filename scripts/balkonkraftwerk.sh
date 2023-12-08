54cc6b90c425d9ea145386e4d874d573a92ff364
r09491 <r09491@gmail.com>
2023-12-08 10:37:09 +0100

    Add Balkon KW estimate script

---
+++ scripts/balkonkraftwerk.sh
@@ -0,0 +1,24 @@
+# Balkonkraftwerke dürfen 4m² per Gesetz nicht überschreiten
+
+# Der Testsieger Solakon SK-011113 hat:
+#  - eine Fläche von 2*1.134*1.722 = 3.905 m²
+#  - einen theoretischen Wirkungsgrad vin 21.8%
+#  - eine berechnete Peak Power von 821Wp. Aber mit 880W beworben!
+#
+# Wirkungsgrade
+#  Diesiger Wintertag: 2
+#  Bewölkter Wintertag: 4?
+#  Blauer Wintertag: 8?
+#  Bewölkter Sommertag: 14?
+#  Blauer Sommertag: 18?
+#
+
+mkdir -p $PANEL_ESTIMATE_STORE_DIR/plot && mkdir -p $PANEL_ESTIMATE_STORE_DIR/csv &&\
+    python3 ../panel_estimate.py --panel_name "Balkon KW Solakon SK-011113" \
+	    --panel_direction 170 \
+	    --panel_slope 25 \
+	    --panel_area 3.905 \
+	    --efficiency 14 \
+	    --threshold 0 \
+	    --plot $PANEL_ESTIMATE_STORE_DIR/plot \
+	    --csv $PANEL_ESTIMATE_STORE_DIR/csv
--- scripts/offgridtech_195W_victron.sh
+++ scripts/offgridtech_195W_victron.sh
@@ -2,11 +2,19 @@
 
 # To asses the the Ecoflow 20 threshold
 
+# Wirkungsgrade
+#  Diesiger Wintertag: 2
+#  Bewölkter Wintertag: 4 ? TBD
+#  Blauer Wintertag: 8 ? TBD
+#  Bewölkter Sommertag: 14 ? TBD
+#  Blauer Sommertag: 18 ? TBD
+#
+
 mkdir -p $PANEL_ESTIMATE_STORE_DIR/plot && mkdir -p $PANEL_ESTIMATE_STORE_DIR/csv &&\
     python3 ../panel_estimate.py --panel_name "Offgridtech 195W" \
 	    --panel_direction 170 \
 	    --panel_slope 25 \
-	    --efficiency 1 \
+	    --efficiency 2 \
 	    --threshold 0 \
 	    --plot $PANEL_ESTIMATE_STORE_DIR/plot \
 	    --csv $PANEL_ESTIMATE_STORE_DIR/csv
