bd96a3232e60764d9059918ff815e151c6fbf094
r09491 <r09491@gmail.com>
2024-06-12 22:18:47 +0200

    Fix

--- scripts/balkonkraftwerk_solix.sh
+++ scripts/balkonkraftwerk_solix.sh
@@ -25,7 +25,7 @@ mkdir -p $SOLAR_PROPHET_STORE_DIR/plot && mkdir -p $SOLAR_PROPHET_STORE_DIR/csv 
 	    --panel_slope 37 \
 	    --panel_area 3.905 \
 	    --panel_efficiency 20 \
-	    --panel_bifacial 40 \
+	    --panel_bifacial 60 \
 	    --panel_albedo 10 \
 	    --system_barrier 40 \
 	    --inverter_limit 600 \
